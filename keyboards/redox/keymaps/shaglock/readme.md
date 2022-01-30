11e877b342032b0e05807426f4ddede385095d15
Ilya Shaplyko <shaplyko.ilya@gmail.com>
2022-01-30 15:38:55 +0100

    test via, add some todos

---
+++ keyboards/redox/keymaps/shaglock/readme.md
@@ -0,0 +1,16 @@
+# A tweaked keymap for Redox
+
+This keymap modifies the [default keymap](https://github.com/qmk/qmk_firmware/tree/master/keyboards/redox) for the awesome [Redox keyboard](https://github.com/mattdibi/redox-keyboard) designed by Mattia Dal Ben.
+
+Used Baltazore's layout as a target. 
+
+![Shaglock's Layout Image]( )
+
+## Changes to the default keymap
+
+Keymap changes from the default keymap are:
+
+- Change default `ESC` into `ESC` on tap, `CTRL` on hold - Love the UNIX-ey `CTRL` key location and wanted to keep `ESC` in the original position
+# TODO: delay?
+# TODO: remove page up/down home end
+# TODO: add delete
---
+++ keyboards/redox/keymaps/shaglock/rules.mk
@@ -0,0 +1,34 @@
+# # MCU name
+# MCU = atmega32u4
+
+# # Bootloader selection
+# BOOTLOADER = caterina
+
+# # Build Options
+# #   change yes to no to disable
+# #
+# BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+# MOUSEKEY_ENABLE = yes       # Mouse keys
+# EXTRAKEY_ENABLE = yes       # Audio control and System control
+# CONSOLE_ENABLE = no         # Console for debug
+# COMMAND_ENABLE = yes        # Commands for debug and configuration
+# NKRO_ENABLE = no            # Enable N-Key Rollover
+# BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+# AUDIO_ENABLE = no           # Audio output
+# RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+
+# SPLIT_KEYBOARD = yes
+
+# DEFAULT_FOLDER = redox/rev1
+
+# # Disable unsupported hardware
+# AUDIO_SUPPORTED = no
+# BACKLIGHT_SUPPORTED = no
+
+
+VIA_ENABLE = yes
+# link time optimization. This will cause the final step to take longer, but should get you a smaller compiled size. This also disables Action Functions, and Action Macros, both of which are deprecated.
+LTO_ENABLE = yes
+
+# tap dance for brackets? finex
+# oneshot modifiers?
